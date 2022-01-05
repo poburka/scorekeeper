21993dcec557d1730a431b7f3fe3118ce27a0b70
Katherine Schaumberg <katherine.schaumberg@gmail.com>
2022-01-05 12:44:10 -0600

    Initial commit

---
+++ R/select_vars.R
@@ -0,0 +1,33 @@
+
+
+#' Select Variables Using Metadata
+#'
+#' @param raw - raw data object
+#' @param metadata - formatted metadata object
+#'
+#' @return tibble with selected variables
+#' @export
+#'
+
+select_vars <- function (raw, metadata){
+  raw_data <- raw
+  metadata <- metadata %>%
+    filter (recode_operation_r == 'select')
+
+#create a new tibble with just the raw data
+  new_table2 <- tibble(raw_data)
+  for (i in 1:nrow(metadata)){
+  select_table <- select_function(raw_tbl = raw, r_vars = r_var_func (metadata, i)) }
+#return the new table
+  return(select_table) }
+
+select_function <- function (raw_tbl, r_vars) {
+  '{r_vars}' <- list (r_vars)
+  raw_tbl <- raw_tbl %>%
+    select(all_of(r_vars))
+  return (raw_tbl) }
+
+r_var_func <- function (metadata, i) {
+  raw_vars <- as.list(el(strsplit(metadata$raw_vars_r[i], ",")))
+  raw_vars <- trimws(raw_vars)
+  return(raw_vars) }
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(scorekeeper)
+
+test_check("scorekeeper")
